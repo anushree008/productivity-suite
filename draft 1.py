23002e3c4c0665a8ae78902143759e1e403a6d96
Anushree <96528824+anushree008@users.noreply.github.com>
2026-05-09 15:20:22 +0200

    Update draft 1.py

--- draft 1.py
+++ draft 1.py
@@ -1,4 +1,10 @@
 import time
-#trial 1
+
+#trial 1 to display time
 current_time_Human_format = time.strftime("%H:%m")
 print(f"Current time:\n{current_time_Human_format}")
+
+#trial 2 for countdown
+# 1500 sec in 25 min
+min,sec = divmod(1500,60)
+print(f"{min:02d}:{sec:02d}")
