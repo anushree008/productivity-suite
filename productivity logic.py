5c61e1fa209ce732b7afcd6764d1a0ff38df1e44
Anushree <96528824+anushree008@users.noreply.github.com>
2026-05-10 19:34:37 +0200

    Update productivity logic.py

--- productivity logic.py
+++ productivity logic.py
@@ -1,6 +1,9 @@
 #defining tasks
 tasks = []
 
+#choice for action
+productivity_choice = int(input("what do you wish to do?\n1. Add new task.\n2.Remove existing task.\n3.View existing tasks.\n: "))
+
 #getting tasks
 task_name = input("Task name:\n")
 task_type = input("Catogory name:\n")
@@ -17,8 +20,8 @@ print(tasks)
 #to print tasks with index numbers
 for index, task in enumerate(tasks):
     print(f"{index}: {task['Name']}")
-choice = int(input("Enter the number of the task to remove: "))
-tasks.pop(choice)
+removing_choice = int(input("Enter the number of the task to remove: "))
+tasks.pop(removing_choice)
 #validation
 print("Task removed.")
 print(tasks)
