29cee3a809a4397b796f415ec4a342dfa447047c
Anushree <96528824+anushree008@users.noreply.github.com>
2026-06-03 09:43:02 +0200

    Update calender trail.py

--- calender trail.py
+++ calender trail.py
@@ -9,7 +9,7 @@ Calendar = customtkinter.CTk()
 Calendar.title("Calender")
 Calendar.geometry("400x300")
 
-#labling the months
+#labling the months and year
 month_label = customtkinter.CTkLabel(Calendar, text="June 2026", font=("Arial", 20))
 month_label.grid(row=0, column=0, columnspan=7, pady=10)
 
