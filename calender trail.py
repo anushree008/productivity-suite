963ee9e169ab66d0eabb5cbe8ea60a161f6e29b4
Anushree <96528824+anushree008@users.noreply.github.com>
2026-06-03 09:48:40 +0200

    Update calender trail.py

--- calender trail.py
+++ calender trail.py
@@ -16,16 +16,15 @@ month_label.grid(row=0, column=0, columnspan=7, pady=10)
 # Day headers
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 for column, day in enumerate(days):
-    customtkinter.CTkLabel(Calendar, text=day).grid(row=1, padx=5)
+    customtkinter.CTkLabel(Calendar, text=day).grid(row=1, column=column, padx=5)#always pass the variables in the .grid
 
 # Date buttons
 month_days = calendar.monthcalendar(2026, 6)  # returns a grid of weeks
 for row_num, week in enumerate(month_days):
     for column_num, day in enumerate(week):
         if day == 0:  # 0 means no day (empty cell)
-            customtkinter.CTkLabel(Calendar, text="").grid(row=row_num+2)
+            customtkinter.CTkLabel(Calendar, text="").grid(row=row_num+2, column=column_num)#always pass the variables in the .grid
         else:
-            customtkinter.CTkButton(Calendar, text=str(day), width=40, height=40).grid(row=row_num+2, padx=2, pady=2)
-
+            customtkinter.CTkButton(Calendar, text=str(day), width=40, height=40).grid(row=row_num+2, column=column_num, padx=2, pady=2)#always pass the variables in the .grid
 
 Calendar.mainloop()
