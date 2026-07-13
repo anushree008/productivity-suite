c5dd27c6f9161b39dba86ca8f9dd75fc85e9c382
Anushree <96528824+anushree008@users.noreply.github.com>
2026-07-13 16:14:22 +0200

    Update draft 5.py

--- draft 5.py
+++ draft 5.py
@@ -484,12 +484,16 @@ class App(customtkinter.CTk):
             #converting data back to string to save in json file
             income_to_save = []
             for i in self.income:
-                new_entry = dict(i)  # makes a copy of the dictionary, not the same one
-                new_entry["Date"] = datetime.datetime.strftime(new_entry["Date"], "%d:%m:%y")
-                income_to_save.append(new_entry)
+                new_entry_income = dict(i)  # makes a copy of the dictionary, not the same one
+                new_entry_income["Date"] = datetime.datetime.strftime(new_entry_income["Date"], "%d:%m:%y")
+                income_to_save.append(new_entry_income)
                 
+            expense_to_save = []    
             for i in self.expense:
-                i["Date"] = datetime.datetime.strftime(i["Date"], "%d:%m:%y")
+                new_entry_expense = dict(i)
+                new_entry_expense["Date"] = datetime.datetime.strftime(new_entry_expense["Date"], "%d:%m:%y")
+                expense_to_save.append(new_entry_expense)
+                
             data = {
                 "tasks": self.tasks,
                 "income": self.income,
